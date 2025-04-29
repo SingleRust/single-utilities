8afd553a00f7e989d27ab5f8bbeedbefc0874884
Ian <ian.diks@uni-saarland.de>
2025-04-29 04:36:40 +0000

    Added threadsafe types for implementation of Numeric and Float Types

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,18 +1,27 @@
-use num_traits::{Bounded, NumCast, One, Zero};
-use std::ops::Add;
+use num_traits::{Bounded, FromPrimitive, NumCast, One, ToPrimitive, Zero};
+use std::ops::{Add, AddAssign, MulAssign};
 
 pub trait NumericOps:
-    Zero + One + NumCast + Copy + std::ops::AddAssign + PartialOrd + Bounded + Add<Output = Self>
+    Zero + One + NumCast + Copy + AddAssign + MulAssign + PartialOrd + Bounded + Add<Output = Self>
 {
 }
 impl<
-    T: Zero + One + NumCast + Copy + std::ops::AddAssign + PartialOrd + Bounded + Add<Output = Self>,
+    T: Zero + One + NumCast + Copy + AddAssign + MulAssign + PartialOrd + Bounded + Add<Output = Self>,
 > NumericOps for T
 {
 }
 
-pub trait FloatOps: NumericOps + num_traits::Float {}
-impl<T: NumericOps + num_traits::Float> FloatOps for T {}
+pub trait NumericOpsTS: NumericOps + Send + Sync {}
+
+impl<T: NumericOps + Send + Sync> NumericOpsTS for T {}
+
+pub trait FloatOps: NumericOps + num_traits::Float + FromPrimitive + ToPrimitive {}
+
+impl<T: NumericOps + num_traits::Float + FromPrimitive + ToPrimitive> FloatOps for T {}
+
+pub trait FloatOpsTS: FloatOps + Sync + Send {}
+
+impl<T: FloatOps + Send + Sync> FloatOpsTS for T {}
 
 // Define a type alias for our numeric constraints
 pub trait NumericNormalize:
